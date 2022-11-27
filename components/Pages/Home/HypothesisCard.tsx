b4d12773adb2ac885a8debd3f46e3b27731cad46
Quantum Snowball <quantum.snowball@gmail.com>
2022-11-27 17:57:49 +0800

    layout: hypothesis statement textfield

--- components/Pages/Home/HypothesisCard.tsx
+++ components/Pages/Home/HypothesisCard.tsx
@@ -1,5 +1,8 @@
 import {
   Card, CardContent,
+  Chip,
+  InputAdornment,
+  TextField,
   Typography
 } from "@mui/material"
 
@@ -8,10 +11,27 @@ function HypothesisCard() {
   return (
     <Card>
       <CardContent>
+        <TextField
+          fullWidth
+          variant="outlined"
+          color='primary'
+          label='What is your Hypothesis?'
+          helperText="Make a guess or forecast about anything"
+          InputProps={{
+            startAdornment:
+              <InputAdornment
+                position="start"
+                sx={{ fontsize: 30 }}
+              >
+                <Chip label='Hypothesis' variant='outlined' color='primary' />
+              </InputAdornment>,
+          }}
+        >
+        </TextField>
+        <Typography variant="h5" sx={{ textAlign: 'left' }} color="text.secondary">Prior:</Typography>
+        <Typography variant="h4">P(H) = ?</Typography>
         <Typography variant="h4" sx={{ textAlign: 'left' }} color="text.secondary">Posterior:</Typography>
         <Typography variant="h3">P(H|E) = 0.863746</Typography>
-        <Typography variant="h5" sx={{ textAlign: 'left' }} color="text.secondary">Prior:</Typography>
-        <Typography variant="h4">P(E) = ?</Typography>
       </CardContent>
     </Card>
   )

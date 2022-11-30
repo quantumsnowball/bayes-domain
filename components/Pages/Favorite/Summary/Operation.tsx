c5926c922e2da59aa48118e5b2bb4f5cce95cf87
Quantum Snowball <quantum.snowball@gmail.com>
2022-11-30 18:36:19 +0800

    feat: press again to delete record

--- components/Pages/Favorite/Summary/Operation.tsx
+++ components/Pages/Favorite/Summary/Operation.tsx
@@ -1,5 +1,5 @@
-import { Box, IconButton } from "@mui/material"
-import { FC } from "react"
+import { Alert, Box, Button, IconButton, Snackbar } from "@mui/material"
+import { FC, useState } from "react"
 import { useDispatch } from "react-redux"
 import { contentActions } from "../../../../redux/slices/contentSlice"
 import { favoriteActions } from "../../../../redux/slices/favoriteSlice"
@@ -10,24 +10,34 @@ import DownloadIcon from '@mui/icons-material/Download'
 
 export const OperationRow: FC<{ content: Content }> = ({ content }) => {
   const dispatch = useDispatch()
+  const [deleteButtonExtended, setDeleteButtonExtended] = useState(false)
   const removeFavorite = (title: string) => dispatch(favoriteActions.removeItem(title))
   const setContent = (content: Content) => dispatch(contentActions.setContent(content))
 
   return (
-    <Box sx={{
-      display: 'flex',
-      flexFlow: 'row nowrap',
-      justifyContent: 'space-between'
-    }}>
-      <IconButton
+    <Box
+      sx={{
+        display: 'flex',
+        flexFlow: 'row nowrap',
+        justifyContent: 'space-between'
+      }}
+      onClick={() => {
+        setDeleteButtonExtended(false)
+      }}
+    >
+      <Button
         color="error"
-        size='small'
+        startIcon={<DeleteIcon />}
         onClick={e => {
+          if (!deleteButtonExtended) {
+            setDeleteButtonExtended(true)
+            e.stopPropagation()
+            return
+          }
           removeFavorite(content.title)
-          e.stopPropagation()
         }}>
-        <DeleteIcon />
-      </IconButton>
+        {deleteButtonExtended ? 'confirm delete' : ''}
+      </Button>
       <IconButton
         color="primary"
         size='small'
